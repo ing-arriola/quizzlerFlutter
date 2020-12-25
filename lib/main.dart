0558ada737573712a699a5c2d2c6b256c3baee47
ing-arriola <ingeniero.arriola@gmail.com>
2020-12-25 16:40:11 -0600

    added a question class to make easier to keep traack of questions and answers

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -26,17 +27,21 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.'
+
+  List<Question> allQuestions = [
+    Question(
+        question: 'You can lead a cow down stairs but not up stairs.',
+        answer: false),
+    Question(
+        question: 'Approximately one quarter of human bones are in the feet.',
+        answer: true),
+    Question(question: 'A slug\'s blood is green.', answer: true),
   ];
-  List<bool> answers = [false, true, true];
 
   int questionNumber = 0;
 
   void nextQuestion(bool answer) {
-    if (answers[questionNumber] == answer) {
+    if (allQuestions[questionNumber].questionAnswer == answer) {
       print('right');
     } else {
       print('wrong');
@@ -60,7 +65,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNumber],
+                allQuestions[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
