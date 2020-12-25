722acd3fae29187d9a43cf44d2ee1dd79fd6475c
ing-arriola <ingeniero.arriola@gmail.com>
2020-12-25 15:01:33 -0600

    Added question and function to go to the next question

--- lib/main.dart
+++ lib/main.dart
@@ -25,15 +25,22 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
-  List<Icon> scoreKeeper = [
-    Icon(Icons.check, color: Colors.green),
-    Icon(Icons.close, color: Colors.red),
-    Icon(Icons.check, color: Colors.green),
-    Icon(Icons.check, color: Colors.green),
-    Icon(Icons.close, color: Colors.red),
-    Icon(Icons.check, color: Colors.green),
+  List<Icon> scoreKeeper = [];
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
   ];
+  int questionNumber = 0;
 
+  void nextQuestion() {
+    setState(() {
+      questionNumber = questionNumber + 1;
+    });
+  }
+
+//Icon(Icons.check, color: Colors.green),
+//     Icon(Icons.close, color: Colors.red),
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -46,7 +53,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -70,7 +77,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
+                nextQuestion();
               },
             ),
           ),
@@ -88,7 +95,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                nextQuestion();
               },
             ),
           ),
